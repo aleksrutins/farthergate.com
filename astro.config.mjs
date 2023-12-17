4f487af8e7788f7318aba7ded16ea5d11c52ed4d
Aleks Rūtiņš <aleks@rutins.com>
2023-12-17 17:53:13 -0500

    configure sentry + spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import sentry from "@sentry/astro";
 import lit from "@astrojs/lit";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit()]
+  integrations: [tailwind(), lit(), sentry({
+    dsn: "https://6b96fa36f8f107f874e2387b573f334c@o4505523519619072.ingest.sentry.io/4506413517832192",
+    sourceMapsUploadOptions: {
+      project: "farthergate",
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }
+  }), spotlightjs()]
 });
