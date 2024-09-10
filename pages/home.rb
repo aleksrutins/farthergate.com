b5d9076a859c68204da474934c780c50ab24fc9b
Aleks Rūtiņš <aleks@rutins.com>
2024-09-09 20:08:36 -0400

    extract project link into function, update link styling

--- pages/home.rb
+++ pages/home.rb
@@ -1,30 +1,41 @@
 class Pages::Home < ::Phlex::HTML
+  def project_link(name:, url:, desc:)
+    li {
+      a(href: url) { name }
+      span { " - " + desc }
+    }
+  end
+
   def view_template
     render ::Pages::Layout.new(:home) {
       p { "i'm aleks rūtiņš." }
-      p { "i'm a student and programmer, skilled with a wide variety of tools." }
+      p { "i'm a student and programmer, skilled with a wide variety of languages and tools." }
 
       h2 { "featured projects" }
       ul {
-        li {
-          a(href: "https://sr.ht/~aleksrutins/phlexite") { "phlexite" }
-          span { " - a simple static site generator using phlex for templating" }
-        }
+        project_link(
+          name: "phlexite",
+          url: "https://sr.ht/~aleksrutins/phlexite",
+          desc: "a simple static site generator using phlex for templating"
+        )
 
-        li {
-          a(href: "https://dynamite.farthergate.com/") { "dynamite" }
-          span { " - a customizable lv2 distortion plugin"}
-        }
+        project_link(
+          name: "dynamite",
+          url: "https://dynamite.farthergate.com/",
+          desc: "a customizable lv2 distortion plugin"
+        )
 
-        li {
-          a(href: "https://packsnap.farthergate.com/") { "packsnap" }
-          span { " - a reproducible container build system" }
-        }
+        project_link(
+          name: "packsnap",
+          url: "https://packsnap.farthergate.com/",
+          desc: "a reproducible container build system"
+        )
 
-        li {
-          a(href: "https://bc.farthergate.com/") { "biocircuits for mere mortals" }
-          span { " - a daunting topic broken down in a (hopefully) friendly way" }
-        }
+        project_link(
+          name: "biocircuits for mere mortals",
+          url: "https://bc.farthergate.com/",
+          desc: "a daunting topic broken down in a (hopefully) friendly way"
+        )
       }
     }
   end
